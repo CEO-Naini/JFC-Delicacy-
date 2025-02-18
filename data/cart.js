9b11268a972db8a0882c64a5a54f390729050374
nainitimothy <nainitimothy343@gmail.com>
2025-02-18 23:42:13 +0300

    save data

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,11 @@
-export const cart = [];
+export const cart = [{
+  productId: 'a82c6bac-3067-4e68-a5ba-d827ac0be010',
+  quantity: 2,
+},
+{
+  productId: '77919bbe-0e56-475b-adde-4f24dfed3a04',
+  quantity:1
+}];
 export function addToCart(productId) {
     let matchingItem;
   
